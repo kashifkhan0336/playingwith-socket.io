0531673397da0760e48e9b21dfbaf500082b4611
kashifkhan0336 <kashifkhan0336@gmail.com>
2020-01-28 01:33:32 +0500

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# playingwith-socket.io
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,22 @@
+var app = require("express")();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", function(socket) {
+  console.log(`a user connected at ${Date()}`);
+  socket.on("disconnect", function() {
+    console.log("user disconnected!");
+  });
+  socket.on("message", function(msg) {
+    console.log(`Message recevied ${msg}`);
+    io.emit("message", `Your Message : ${msg}`);
+  });
+});
+
+http.listen(3000, function() {
+  console.log("listening on *:3000");
+});
